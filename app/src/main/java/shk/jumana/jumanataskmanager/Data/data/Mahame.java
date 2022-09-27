85d901255412be71b36d08e650a84dda44b8287b
Owner <alsalamcse>
2022-09-27 11:38:40 +0300

    menu

---
+++ app/src/main/java/shk/jumana/jumanataskmanager/Data/data/Mahame.java
@@ -0,0 +1,71 @@
+package shk.jumana.jumanataskmanager.Data.data;
+
+public class Mahame
+{
+
+    private String key;//المزهي لكل المهمات-رقم مميز -رقم هوية -id
+    private String title;
+    private String subject;
+    private int importance;  //for the seek bar
+    private String owners;
+
+
+
+
+
+    public Mahame()
+    {
+
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getImportance() {
+        return importance;
+    }
+
+    public void setImportance(int importance) {
+        this.importance = importance;
+    }
+
+    public String getOwners() {
+        return owners;
+    }
+
+    public void setOwners(String owners) {
+        this.owners = owners;
+    }
+
+    @Override
+    public String toString() {
+        return "Mahame{" +
+                "key='" + key + '\'' +
+                ", title='" + title + '\'' +
+                ", subject='" + subject + '\'' +
+                ", importance=" + importance +
+                ", owners='" + owners + '\'' +
+                '}';
+    }
+}
