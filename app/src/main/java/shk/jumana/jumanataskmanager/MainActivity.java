724e5726540faadf5de39ccb1d4b9ff7a7dea9dc
Owner <alsalamcse>
2022-09-27 13:22:40 +0300

    menu

--- app/src/main/java/shk/jumana/jumanataskmanager/MainActivity.java
+++ app/src/main/java/shk/jumana/jumanataskmanager/MainActivity.java
@@ -1,8 +1,12 @@
 package shk.jumana.jumanataskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///ljlkjlkj
+
+
+        //دالة مسؤولة عن فحص و تشغيل ال menu
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);//بتحط المحل الي الها يعني اللا R هو اللا res يعني resourse
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.itmSettings) {
+            Intent i = new Intent(MainActivity.this, settings.class);
+            startActivity(i);
+        }
+        if (item.getItemId() == R.id.itmHistory)
+        {
+            Intent i = new Intent(MainActivity.this, History.class);
+            startActivity(i);
+        }
+
+            return true;
+
+
+    }
+
+
+
+
+
 }
+
+
+
