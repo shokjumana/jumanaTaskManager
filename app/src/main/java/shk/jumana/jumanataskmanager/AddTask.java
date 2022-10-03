c3a904de9e714104671a062b740cab1f35556173
shokjumana <jumanashok55@gmail.com>
2022-10-03 23:40:42 +0300

    nothing new

--- app/src/main/java/shk/jumana/jumanataskmanager/AddTask.java
+++ app/src/main/java/shk/jumana/jumanataskmanager/AddTask.java
@@ -3,12 +3,29 @@ package shk.jumana.jumanataskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddTask extends AppCompatActivity {
 
+    private TextInputEditText etTittleTask;
+    private TextInputEditText etSubjectTask;
+    private SeekBar skImportant;
+    private ImageButton imageTask;
+    private Button btnCancelTask;
+    private Button btnSaveTask;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+
+
+
     }
 }
--- app/src/main/java/shk/jumana/jumanataskmanager/MainActivity.java
+++ app/src/main/java/shk/jumana/jumanataskmanager/MainActivity.java
@@ -8,7 +8,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
